b6a89604f6a3577778a9c2ddfefd28f5a3109c3e
charon@ <oncwnuCOZ5d0_sYjnJm7jghcTbcQ@git.weixin.qq.com>
2020-05-02 18:12:44 +0800

    char是整数也是字符,能运算 ASCII

---
+++ zifuleixing.c
@@ -0,0 +1,110 @@
+/*字符类型  char 是整数也是类型  字符
+用单引号表示的字符字面量 'a' '1'
+'' 也是一个子符
+printf 和 scanf 里用 %c 输入与输出字符 
+
+
+*/
+ 
+/*
+#include <stdio.h>
+
+int main()
+{
+	char c;
+	char d;
+	c = 1;
+	d = '1';
+	if ( c == d) {
+		printf("xiangdeng");
+	} else {
+		printf("不相等\n"); 
+	}
+	printf("c=%d\n",c);
+	printf("d= %d\n",d);
+ } 
+ */
+ 
+/* 
+#include <stdio.h>
+
+int main()
+{
+	char c;
+	scanf("%c",&c);
+	printf("c=%d\n",c);
+	printf("c= '%c'\n",c);
+	
+	return 0; 
+ } 
+ */
+ 
+ 
+ /* 
+#include <stdio.h>
+
+int main()
+{
+	int i;   //字符的ASCII编码 
+	char c;
+	scanf("%d",&i);
+	c = i;
+	printf("c=%d\n",c);
+	printf("c= '%c'\n",c);
+	
+	return 0; 
+ } 
+ 
+  */
+ /* 
+#include <stdio.h>
+
+int main()
+{
+	if ( 49 == '1' ) { //ASCII编码和字符  
+		printf("OK");
+	}	
+	return 0; 
+ } 
+ */
+ 
+ /* 
+ //混合输入
+ #include <stdio.h>
+
+int main()
+{
+	int i;
+	char c;
+	scanf("%d %c",&i,&c); //%d 没有空格 只读到整数  
+	printf("i=%d,c=%d,c='%c'\n",i,c,c);	
+	return 0; 
+ } 
+ */
+ 
+//字符计算 
+#include <stdio.h>
+/*
+int main()
+{
+
+	char c = 'A';
+	c++;
+	printf("%c\n",c); //B
+	return 0; 
+ }
+ */
+ int main()
+{
+
+	int i = 'Z'-'A';
+	
+	printf("%d\n",i); //25
+	return 0; 
+ }  
+ 
+ //一个字符加一个数字得到ASCII码表的那个数之后的字符 
+//  两个字符的减 得到他们在表中的距离  
+// 0-9是顺序排列的 
+ //字母在ASCII中是顺序排列的
+ //大写字母和小写字母是分开排列的,不在一起 
